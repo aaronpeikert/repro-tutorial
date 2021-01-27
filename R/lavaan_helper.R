d0bca6e4abeaef02ac70f4b3b8eac1e0ff095a16
Aaron Peikert <aaron.peikert@posteo.de>
2021-01-27 12:02:31 +0100

    write more lavaan helper

--- R/lavaan_helper.R
+++ R/lavaan_helper.R
@@ -1,3 +1,7 @@
+items <- function(item, n){
+  stringr::str_c(item, seq_len(n))
+}
+
 intercepts <- function(variables, intercepts){
   stopifnot(is.character(variables))
   stringr::str_c(variables, " ~ ", modifier(intercepts), "1")
@@ -24,4 +28,20 @@ measurement <- function(latent, variables, loadings){
   stringr::str_c(latent, " =~ ", variables)
 }
 
+varying_modifer <- function(modifier, n, rel_change, abs_change){
+  if(missing(n))n <- ifelse(missing(abs_change), length(rel_change), length(abs_change))
+  pos <- sample(seq_along(modifier), n)
+  varied <- modifier
+  if(missing(abs_change)){
+    varied[pos] <- modifier[pos] * (1 + rel_change)
+    return(list(modifier, varied))
+  } else {
+    varied[pos] <- modifier[pos] + abs_change
+    return(list(modifier, varied))
+  }
+}
 
+combine <- function(...)stringr::str_c(..., sep = "\n", collapse = "\n")
+shuffle <- function(x)sample(x, length(x))
+pad <- function(x, replace, length)c(x, rep(replace, length))
+pad_zero <- function(x, length)pad(x, 0, length)
