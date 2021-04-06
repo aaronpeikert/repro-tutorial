121ac1b1e1799d635d33bd51d746bf6fef58130c
Maximilian-Stefan-Ernst <maximilian.stefan.ernst@gmail.com>
2021-04-06 15:32:41 +0200

    add basic data and models for simulation

--- R/simulation.R
+++ R/simulation.R
@@ -1,4 +1,5 @@
 source(here::here("R", "lavaan_helper.R"))
+library(lavaan)
 loadings_jones_paulhus <- c(38, 31, 40, 52, 59, 71, 62, 46, 51)/100
 cohend_jones_paulhus <- c(24, 29, 35)/100
 #----model-truth----
@@ -11,10 +12,37 @@ model_truth <- combine(
   intercepts("MACH", list(0, 0.1)),
   variances("MACH", list(1, 1))
 )
-#----model----
+#----models----
+model_same <- 
+  "MACH =~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
+  MACH ~ c(a,a)*1
+  x1 ~ 0*1"
+
+model_differ <- 
+  "MACH =~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
+  MACH ~ c(a,b)*1
+  x1 ~ 0*1"
 
 
 #----generate----
+n = 3000 
+data <- lavaan::simulateData(
+  model_truth,
+  sample.nobs = c(n/2, n/2), 
+  standardized = TRUE)
+
+#----analyze----
+res_same <- cfa(model_same, data, group = "group", group.equal = c("loadings", "intercepts"))
+
+summary(res_same)
+
+res_differ <- cfa(model_differ, data, group = "group", group.equal = c("loadings", "intercepts"))
+
+summary(res_differ)
+
+
+#----some functions----
+
 generate <- function(n, diff, vary_intercepts, vary_loadings, skewness, ...){
   latent <- "MACH"
   items <- items("x", 9)
@@ -32,7 +60,7 @@ generate <- function(n, diff, vary_intercepts, vary_loadings, skewness, ...){
   skewness <- shuffle(pad_zero(skewness, length(items)))
   lavaan::simulateData(model, skewness = skewness, sample.nobs = c(n/2, n/2), standardized = TRUE, ...)
 }
-#----analyze----
+
 analyze <- function(simulated){
   latent <- "MACH"
   items <- items("x", 9)
