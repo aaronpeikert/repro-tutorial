a62eff1849329f761cf87f63f11e5493777cd639
Aaron Peikert <aaron.peikert@posteo.de>
2021-05-10 19:01:41 +0200

    revamp simulation still WIP

--- R/hash.R
+++ R/hash.R
@@ -1,6 +1,6 @@
 #----hash----
 # compute hash using md5
 hash <- digest::digest(res_raw, "md5")
-if(hash != "4203854848ad0cc5d2f848c44b70815d"){
+if(hash != "cd14a91f680aef9355d8b7fe99e7f20f"){
   warning("Mismatch between original and current simulations!\nHash now is:\n    '", hash, "'")
 }
