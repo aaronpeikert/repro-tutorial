5dc5549003977c0be12f0b3929cfa126832700a4
Aaron Peikert <aaron.peikert@posteo.de>
2021-05-13 10:42:00 +0200

    use hash hash is incorrect, because simulation is running

--- R/hash.R
+++ R/hash.R
@@ -1,6 +1,6 @@
 #----hash----
 # compute hash using md5
-hash <- digest::digest(res_raw, "md5")
-if(hash != "cd14a91f680aef9355d8b7fe99e7f20f"){
+hash <- digest::digest(res, "md5")
+if(hash != "e9e0dfe4b4753c1e00bb02e4205d8772"){
   warning("Mismatch between original and current simulations!\nHash now is:\n    '", hash, "'")
 }
