791f2b39828413732571ff3ff6e21a81f8ecdc20
Aaron Peikert <aaron.peikert@posteo.de>
2021-06-30 09:44:33 +0200

    dont mess around with seeds

--- R/simulation.R
+++ R/simulation.R
@@ -1,24 +1,4 @@
-with_lecuyer <- function(seed, code) {
-  withr::with_seed(
-    seed,
-    code,
-    .rng_kind = "L'Ecuyer-CMRG"
-  )
-}
-
-generate_seeds <- function(k, seed) {
-  with_lecuyer(
-    seed,
-    purrr::accumulate(seq_len(k), function(x, y) # discard y
-      parallel::nextRNGStream(x), .init = .Random.seed)
-  )
-}
-
-error <- function(i){
-  as.numeric(scale(rnorm(i), scale = FALSE)) # only centered
-}
-
-simulate_data_ <- function(n, df, d, i){
+simulate_data <- function(n, df, d, i){
   stopifnot(n %% 2L == 0L)
   group <- rep(c(0L, 1L), each = n/2)
   rand <-  matrix(rchisq(n * i, df/i), ncol = i) # sum of n chisq has df of n*df
@@ -26,11 +6,6 @@ simulate_data_ <- function(n, df, d, i){
   colnames(effect) <- stringr::str_c("x", seq_len(i))
   dplyr::mutate(tibble::as_tibble(effect),
                 group = group)
-}
-
-simulate_data <- function(seed, n, df, d, i = 9, ...){
-  stopifnot(length(list(...)) == 0L)
-  with_lecuyer(seed, simulate_data_(n, df, d, i))
 }
 
 planned_analysis <- function(data){
@@ -59,16 +34,14 @@ simulation_study <- function(setup){
 }
 
 setup <- tidyr::expand_grid(
-  seed = generate_seeds(10, 1002),
   n = seq(10, 1000, 10),
   df = 8, # skew = sqrt(8/df)
-  d = seq(0, .5, 0.1)
-)
+  d = seq(0, .5, 0.1),
+  i = 10)
 
 res <- simulation_study(setup)
 
 test <- res %>% 
-  select(-seed) %>% 
   group_by(across(-results)) %>% 
   unnest_wider(results) %>% 
   summarise(power = mean(p_value < 0.05), .groups = "drop")
