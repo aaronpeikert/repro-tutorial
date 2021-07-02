cac919d89aee427b8c3d9c73d97dcca9ed3173c4
Aaron Peikert <aaron.peikert@posteo.de>
2021-07-02 10:17:27 +0200

    verify that simulation works

--- R/simulation.R
+++ R/simulation.R
@@ -2,23 +2,24 @@ simulate_data <- function(n, df, d, i){
   stopifnot(n %% 2L == 0L)
   group <- rep(c(0L, 1L), each = n/2)
   rand <-  matrix(rchisq(n * i, df/i), ncol = i) # sum of n chisq has df of n*df
-  effect <- rand + d * group
+  d_scaled <- d * sqrt(2*df)/i # scale d to express sd units (var(chisq) = 2df)
+  #browser()
+  effect <- rand + d_scaled * group
   colnames(effect) <- paste0("x", seq_len(i))
   effect <- as.data.frame(effect)
   effect$group <- group
   return(effect)
 }
 
-planned_analysis <- function(data){
+planned_analysis <- function(data, use_rank = "skew"){
   x <- rowMeans(data["group" != names(data)])
   y <- as.factor(data$group)
   skew <- moments::skewness(x)
   # skewness cutoff
-  use_rank <- abs(skew) > 1
+  if(use_rank == "skew")use_rank <- abs(skew) > 1
   if(use_rank){
     x <- rank(x)
   }
-  x <- scale(x)
   test <- t.test(x ~ y)
   list(test = test, skew = skew, use_rank = use_rank, n = length(x))
 }
@@ -27,6 +28,27 @@ extract_results <- function(analysis){
   list(p_value = analysis$test$p.value,
        skew = analysis$skew)
 }
+
+t2d <- function(t, n1, n2)t * sqrt(((n1 + n2)/(n1 * n2)) * (n1 + n2)/(n1 + n2 -2))
+parameter_recovery <- function(n, df, d, i, rank = FALSE){
+  t2d(planned_analysis(simulate_data(n, df, d, i), rank)$test$statistic, n/2, n/2)
+}
+
+test <- simulate_data(1000000, 100, 0.1, 10)
+m1 <- filter(test, group == 1) %>% 
+  select(-group )%>% 
+  rowSums() %>% 
+  mean()
+m0 <- filter(test, group == 0) %>% 
+  select(-group )%>% 
+  rowSums() %>% 
+  mean()
+sd <- test %>% rowSums() %>% sd()
+
+(m1 - m0)/sd
+
+t2d(planned_analysis(test)$test$statistic, 1000000/2, 1000000/2)
+parameter_recovery(1000000, 100, 0.1, 10)
 
 simulation_study <- function(setup){
   all_steps <- purrr::compose(extract_results, planned_analysis, simulate_data)
